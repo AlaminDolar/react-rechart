807d356d0e46fb3f293fde9fc54f5aac8ff9386c
AlaminDolar <amin15-9898@diu.edu.bd>
2022-08-22 01:19:28 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import MyLineChart from './components/MyLineChar/MyLineChart';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MyLineChart></MyLineChart>
     </div>
   );
-}
+};
 
 export default App;
