527a65e33e7074bf983f7fa38fe77f483304c465
AlaminDolar <amin15-9898@diu.edu.bd>
2022-08-22 02:52:42 +0600

    Barchart Aded

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import MyLineChart from './components/MyLineChar/MyLineChart';
+import SpecialChart from './components/SpecialChart/SpecialChart';
 
 const App = () => {
   return (
     <div>
       <MyLineChart></MyLineChart>
+      <SpecialChart></SpecialChart>
     </div>
   );
 };
